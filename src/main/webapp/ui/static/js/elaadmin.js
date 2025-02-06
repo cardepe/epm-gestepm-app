38d49ee5dcf4ac5ce01007b5c20ff73a74f9aa72
Carlos Devesa Pérez <carlosdevesaperez@hotmail.com>
2025-02-06 15:51:43 +0100

    [fix] Responsive view bug

--- src/main/webapp/ui/static/js/elaadmin.js
+++ src/main/webapp/ui/static/js/elaadmin.js
@@ -1,90 +1,69 @@
 $.noConflict();
 
-jQuery(document).ready(function($) {
+function checkDeviceSize() {
+    const windowWidth = $(window).width();
+    $('body').toggleClass('small-device', windowWidth < 1200);
+}
 
-	"use strict";
+function toggleSearch(open) {
+    $('.search-trigger').parent('.header-left').toggleClass('open', open);
+}
 
-	[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {
-		new SelectFx(el);
-	});
+function toggleMenu() {
+    const windowWidth = $(window).width();
+    const $body = $('body');
+    const $leftPanel = $('#left-panel');
 
-	jQuery('.selectpicker').selectpicker;
+    if (windowWidth < 1200) {
+        $body.removeClass('open');
+        windowWidth < 768 ? $leftPanel.slideToggle() : $leftPanel.toggleClass('open-menu');
+    } else {
+        $body.toggleClass('open');
+        $leftPanel.removeClass('open-menu');
+    }
+}
 
+function addDropdownSubtitle() {
+    $(".menu-item-has-children.dropdown").each(function () {
+        const $dropdown = $(this);
+        const tempText = $dropdown.children('.dropdown-toggle').html();
+        if (!$dropdown.find('.subtitle').length) {
+            $dropdown.children('.sub-menu').prepend(`<li class="subtitle">${tempText}</li>`);
+        }
+    });
+}
 
-	
+jQuery(document).ready(function ($) {
+    "use strict";
 
-	$('.search-trigger').on('click', function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-		$('.search-trigger').parent('.header-left').addClass('open');
-	});
+    // Inicializar selectores personalizados
+    $('select.cs-select').each(function () {
+        new SelectFx(this);
+    });
 
-	$('.search-close').on('click', function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-		$('.search-trigger').parent('.header-left').removeClass('open');
-	});
+    let selectpicker = $('.selectpicker');
+    if (selectpicker.length > 0) {
+        selectpicker.selectpicker();
+    }
 
-	$('.equal-height').matchHeight({
-		property: 'max-height'
-	});
+    // Eventos
+    $('.search-trigger').on('click', function (event) {
+        event.preventDefault();
+        toggleSearch(true);
+    });
 
-	// var chartsheight = $('.flotRealtime2').height();
-	// $('.traffic-chart').css('height', chartsheight-122);
+    $('.search-close').on('click', function (event) {
+        event.preventDefault();
+        toggleSearch(false);
+    });
 
+    $('.equal-height').matchHeight({property: 'max-height'});
 
-	// Counter Number
-//	$('.count').each(function () {
-//		$(this).prop('Counter',0).animate({
-//			Counter: $(this).text()
-//		}, {
-//			duration: 3000,
-//			easing: 'swing',
-//			step: function (now) {
-//				$(this).text(Math.ceil(now));
-//			}
-//		});
-//	});
+    $('#menuToggle').on('click', toggleMenu);
 
+    $(".menu-item-has-children.dropdown").on('click', addDropdownSubtitle);
 
-	 
-	 
-	// Menu Trigger
-	$('#menuToggle').on('click', function(event) {
-		var windowWidth = $(window).width();   		 
-		if (windowWidth<1200) { 
-			$('body').removeClass('open'); 
-			if (windowWidth<768){ 
-				$('#left-panel').slideToggle(); 
-			} else {
-				$('#left-panel').toggleClass('open-menu');  
-			} 
-		} else {
-			$('body').toggleClass('open');
-			$('#left-panel').removeClass('open-menu');  
-		} 
-			 
-	}); 
-
-	 
-	$(".menu-item-has-children.dropdown").each(function() {
-		$(this).on('click', function() {
-			var $temp_text = $(this).children('.dropdown-toggle').html();
-			$(this).children('.sub-menu').prepend('<li class="subtitle">' + $temp_text + '</li>'); 
-		});
-	});
-
-
-	// Load Resize 
-	$(window).on("load resize", function(event) { 
-		var windowWidth = $(window).width();  		 
-		if (windowWidth<1200) {
-			$('body').addClass('small-device'); 
-		} else {
-			$('body').removeClass('small-device');  
-		} 
-		
-	});
-  
- 
+    // Ejecutar verificaciones iniciales
+    checkDeviceSize();
+    $(window).on('resize', checkDeviceSize);
 });
