836bcda9431b345874a1bd6ca6cea4dd0949024a
Carlos Devesa Pérez <devesaperezcarlos@gmail.com>
2025-05-08 20:58:31 +0200

    [fix] No programmed share files

--- gestepm-model/src/main/java/com/epm/gestepm/model/personalexpense/dao/constants/PersonalExpenseAttributes.java
+++ gestepm-model/src/main/java/com/epm/gestepm/model/personalexpense/dao/constants/PersonalExpenseAttributes.java
@@ -24,6 +24,4 @@ public abstract class PersonalExpenseAttributes {
 
   public static final String ATTR_PE_PAYMENT_TYPE = "attr_pe_payment_type";
 
-  public static final String ATTR_PE_FILE_ID = "attr_pe_file_id";
-
 }
