961f04cca4c2559a8b9ec4091a07795e9e85b665
Carlos Devesa Pérez <devesaperezcarlos@gmail.com>
2025-05-14 18:45:42 +0200

    [fix] Start date before end date in shares update

--- gestepm-model/src/main/java/com/epm/gestepm/model/shares/common/checker/ShareDateChecker.java
+++ gestepm-model/src/main/java/com/epm/gestepm/model/shares/common/checker/ShareDateChecker.java
@@ -1,5 +1,6 @@
 package com.epm.gestepm.model.shares.common.checker;
 
+import com.epm.gestepm.modelapi.shares.common.exception.StartEndDateException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,13 @@ public class ShareDateChecker {
 
     private static final Integer MAX_HOURS = 8;
 
-    public LocalDateTime checker(final LocalDateTime startDate, final LocalDateTime endDate) {
+    public void checkStartBeforeEndDate(final LocalDateTime start, final LocalDateTime end) {
+        if (end != null && start.isAfter(end)) {
+            throw new StartEndDateException(start, end);
+        }
+    }
+
+    public LocalDateTime checkMaxHours(final LocalDateTime startDate, final LocalDateTime endDate) {
         final LocalDateTime maxDateToEnd = startDate.plusHours(MAX_HOURS);
 
         return maxDateToEnd.isBefore(endDate) ? maxDateToEnd : endDate;
