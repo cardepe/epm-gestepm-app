c3da0589de255472c217088a314d04300337f349
argenis <argenismrvel@gmail.com>
2025-08-15 09:44:26 -0400

    [UPDATE] Warehouse JS and messages.propertis

--- src/main/webapp/ui/static/js/pages/signings/warehouse.js
+++ src/main/webapp/ui/static/js/pages/signings/warehouse.js
@@ -3,21 +3,21 @@ const endpoint = '/v1/signings/warehouse';
 let currentWarehouse = null;
 
 function initializeDataTables() {
-    const columns = ['id', 'project.name', 'startedAt', 'closedAT'];
+    const columns = ['id', 'project.name', 'startedAt', 'closedAt', 'id'];
 
     const actions = [
         {
             action: 'view'
-            , url: 'signing/warehouse/{id}'
+            , url: '/signings/warehouse/{id}'
             , permission: 'edit_warehouse_signings'
         }
     ];
 
     const expand = ['project'];
 
-    const filter = [{'userIds': userId, 'current': false}];
+    const filters = [{'userIds': userId, 'current': false}];
 
-    const columnDef = [
+    const columnDefs = [
         {
             targets: [2, 3]
             , render: function(data) {
@@ -31,4 +31,41 @@ function initializeDataTables() {
             }
         }
     ];
+
+    customDataTable = new CustomDataTable(columns, endpoint, null, actions, expand, filters, columnDefs);
+    dTable = createDataTable('#dTable', customDataTable, locale);
+    customDataTable.setCurrentTable(dTable);
+}
+
+function initializeSelects() {
+    const form = document.getElementById('form');
+    const selects = form.querySelectorAll('select');
+
+    selects.forEach(select => {
+        const select2 = createBasicSelect2($(select));
+
+        if (currentWarehouse)
+        {
+            select2.append(new Option(currentWarehouse.project.name, currentWarehouse.project.id, true, true));
+        }
+    });
+}
+
+async function getCurrentWarehouse() {
+    await axios.get(endpoint, {
+        params: {
+            userIds: userId
+            , current: true
+            , offset: 0
+            , limit: 1
+            , _expand: 'project'
+        }
+    }).then((response) => {
+        currentWarehouse = response.data.data[0];
+        updateForm();
+    }).catch(error => showNotify(error.response.data.detail, 'danger'));
+}
+
+function updateForm() {
+
 }
